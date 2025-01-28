bf8811e46d9134e303ee4cd3bfb45e75b3d12d85
jakir9324 <jakirhusen9324@gmail.com>
2025-01-28 10:55:27 +0600

    variable

---
+++ script.js
@@ -0,0 +1,39 @@
+
+//general way
+//summation
+
+x = 400;
+y = 600;
+x+y;
+console.log(x+y);
+
+//3.using let
+let a = 300;
+let b = 400;
+let result = a+b;
+console.log(result)
+
+//subtraction---
+let numberOne = 180;
+let numberTwo = 90;
+let sub =numberOne-numberTwo;
+console.log(sub);
+
+//multiplication---
+let digit1 = 30;
+let digit3 = 12;
+let mul =digit1*digit3;
+console.log(mul);
+
+//division---
+let data1 = 60;
+let data2 = 5;
+let div = data1/data2;
+console.log(div);
+
+//remainder--
+let data3 = 5;
+let data4 = 3;
+let rem = data3%data4;
+console.log(rem);
+
